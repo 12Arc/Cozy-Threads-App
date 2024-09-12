e5c861e290e1a50fc3231c5f0f8d42d8a72a5b18
12Arc <143301288+12Arc@users.noreply.github.com>
2024-09-11 22:39:33 -0400

    Prepare for Heroku deployment with ES6 modules

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //config
 dotenv.config();
 
@@ -20,6 +23,13 @@ app.use(cors());
 //middleware
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+// Catch-all route to serve the index.html from the 'dist' directory
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
 
 //routes
 app.use("/api/auth", authRoutes);
