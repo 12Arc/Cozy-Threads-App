211c0d5bb5ee3e193f060d0b9c7546397fa597e1
12Arc <143301288+12Arc@users.noreply.github.com>
2024-09-12 02:50:46 -0400

    vite depth vite

--- client/src/components/Layout/Footer.jsx
+++ client/src/components/Layout/Footer.jsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <div className="footer">
-      <h1 className="text-center">All Right Reserved &copy; Techinfoyt</h1>
+      <h1 className="text-center">All Right Reserved &copy; Cozy Threads</h1>
     </div>
   );
 };
